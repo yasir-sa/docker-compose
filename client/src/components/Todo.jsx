df2ededc603e1f1f597c69030047bceab47e80d8
asiyasa <asiyasa07@gmail.com>
2026-06-17 09:09:07 +0530

    test: checking jenkins auto trigger

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -86,7 +86,7 @@ function Todo() {
     <div className="todo-wrapper">
       <div className="todo-card">
         <div className="todo-header">
-          <h1>My Todo List</h1>
+          <h1>yasir Todo List</h1>
           <p>{completed} of {todos.length} {todos.length === 1 ? 'task' : 'tasks'} completed</p>
         </div>
 
